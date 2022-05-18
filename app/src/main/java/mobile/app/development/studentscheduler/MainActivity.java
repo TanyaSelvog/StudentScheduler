c6982af9fa809e15fe246a83e48e63315d78a160
Tanya Selvog <tanyaselvog@gmail.com>
2022-05-18 15:19:33 -0700

    Initial commit

---
+++ app/src/main/java/mobile/app/development/studentscheduler/MainActivity.java
@@ -0,0 +1,14 @@
+package mobile.app.development.studentscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
