98984ebf12275b398c4c9f4fc3e80a2b16e3ded8
Tanya Selvog <tanyaselvog@gmail.com>
2022-06-01 17:43:49 -0700

    working on TermAdapter

--- app/src/main/java/mobile/app/development/studentscheduler/UI/TermAdapter.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/TermAdapter.java
@@ -1,4 +1,63 @@
 package mobile.app.development.studentscheduler.UI;
 
-public class TermAdapter {
-}
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import mobile.app.development.studentscheduler.Entity.Term;
+import mobile.app.development.studentscheduler.R;
+
+public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder>{
+    class TermViewHolder extends RecyclerView.ViewHolder{
+        private final TextView termItemView;
+        private TermViewHolder(View itemView){
+            super(itemView);
+            termItemView =itemView.findViewById(R.id.textView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position=getAdapterPosition();
+                    final Term current = mTerms.get(position);
+                    Intent intent = new Intent(context,CourseList.class);
+                    intent.putExtra("termName", current.getTermName());
+
+                }
+            });
+        }
+    }
+    private List<Term> mTerms;
+    private final Context context;
+    private final LayoutInflater mInflater;
+    public TermAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public TermAdapter.TermViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermAdapter.TermViewHolder holder, int position) {
+
+    }
+    public void setTerms(List<Term> terms){
+        mTerms=terms;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mTerms != null)
+        return mTerms.size();
+    else return 0;
+}}
