795cae723a9a5df164e0574b7c6e6750e062cef7
Tanya Selvog <tanyaselvog@gmail.com>
2022-06-04 17:31:23 -0700

    working on New Course layout

--- app/src/main/java/mobile/app/development/studentscheduler/UI/NewCourse.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/NewCourse.java
@@ -2,5 +2,25 @@ package mobile.app.development.studentscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import mobile.app.development.studentscheduler.R;
+
+
 public class NewCourse extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_course);
+    }
+
+
+
+
+
 }
