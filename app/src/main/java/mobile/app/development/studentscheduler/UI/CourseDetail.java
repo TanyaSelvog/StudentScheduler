35fb8daa584c0189970383797b63e7f5d252f431
Tanya Selvog <tanyaselvog@gmail.com>
2022-06-07 17:17:48 -0700

    working on Spinner in NewCourse

--- app/src/main/java/mobile/app/development/studentscheduler/UI/CourseDetail.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/CourseDetail.java
@@ -2,6 +2,10 @@ package mobile.app.development.studentscheduler.UI;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,13 +14,18 @@ import mobile.app.development.studentscheduler.R;
 public class CourseDetail extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
-    }
 
+
+    }
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_course_notes, menu);
         return true;
     }
+
+
+
+
 }
--- app/src/main/java/mobile/app/development/studentscheduler/UI/NewCourse.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/NewCourse.java
@@ -3,7 +3,9 @@ package mobile.app.development.studentscheduler.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -12,15 +14,41 @@ import android.widget.Toast;
 import mobile.app.development.studentscheduler.R;
 
 
-public class NewCourse extends AppCompatActivity {
+public class NewCourse extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_course);
+        Spinner spinner = (Spinner) findViewById(R.id.status_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.status_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+      String item= (String) parent.getItemAtPosition(pos);
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
     }
 
-
-
-
-
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 }
+
+
+
+
+
+
+
