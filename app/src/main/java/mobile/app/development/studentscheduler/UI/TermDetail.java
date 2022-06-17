e76e9288aad9059cd8a7aa861006874e549f4914
Tanya Selvog <tanyaselvog@gmail.com>
2022-06-17 14:15:25 -0700

    added EditText (title, startDate, endDate) to TermDetail

--- app/src/main/java/mobile/app/development/studentscheduler/UI/TermDetail.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/TermDetail.java
@@ -1,17 +1,35 @@
 package mobile.app.development.studentscheduler.UI;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import mobile.app.development.studentscheduler.R;
 
 public class TermDetail extends AppCompatActivity {
+    EditText editTitle;
+    EditText editStartDate;
+    EditText editEndDate;
+    String title;
+    String startDate;
+    String endDate;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_term_detail);
+            editTitle=findViewById(R.id.editTitle);
+            editStartDate=findViewById(R.id.editStartDate);
+            editEndDate=findViewById(R.id.editEndDate);
+            title=getIntent().getStringExtra("termName");
+            startDate=getIntent().getStringExtra("startTermDate");
+            endDate=getIntent().getStringExtra("endTermDate");
+            editTitle.setText(title);
+            editStartDate.setText(startDate);
+            editEndDate.setText(endDate);
+
+
 
 
         }
