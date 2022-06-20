7bab84a3f5d2ee1b5860cadcb1d0e6f03639fe6d
Tanya Selvog <tanyaselvog@gmail.com>
2022-06-20 14:35:13 -0700

    added onCreate to AssessmentDetail

--- app/src/main/java/mobile/app/development/studentscheduler/UI/AssessmentDetail.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/AssessmentDetail.java
@@ -1,6 +1,19 @@
 package mobile.app.development.studentscheduler.UI;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import mobile.app.development.studentscheduler.R;
+
 public class AssessmentDetail extends AppCompatActivity {
-}
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_assessment_detail);
+
+
+        }}
+
