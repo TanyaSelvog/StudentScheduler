e5e3e6f30ca3b200e8291f6ce8dc0ad62a475457
Tanya Selvog <tanyaselvog@gmail.com>
2022-06-27 13:38:32 -0700

    working on adding onSave button to NewTerms and copied over TermDetail data to NewTerms

--- app/src/main/java/mobile/app/development/studentscheduler/UI/NewTerms.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/NewTerms.java
@@ -4,15 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
+import mobile.app.development.studentscheduler.DB.Repository;
+import mobile.app.development.studentscheduler.Entity.Term;
 import mobile.app.development.studentscheduler.R;
 
 public class NewTerms extends AppCompatActivity {
+    EditText editTitle;
+    EditText editStartDateText;
+    EditText editEndDateText;
+    String title;
+    String startDate;
+    String endDate;
+    Repository repo;
+    int termID;
 
+    //C&P over term_detail and modifying it (will need to edit/change later 6.27
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_terms);
+            editTitle=findViewById(R.id.editTitle);
+            editStartDateText=findViewById(R.id.startDateText);
+            editEndDateText=findViewById(R.id.endDateText);
+            termID=getIntent().getIntExtra("termID", -1);
+            title=getIntent().getStringExtra("termName");
+            startDate=getIntent().getStringExtra("startTermDate");
+            endDate=getIntent().getStringExtra("endTermDate");
+            editTitle.setText(title);
+            editStartDateText.setText(startDate);
+            editEndDateText.setText(endDate);
+            repo = new Repository(getApplication());
+
+
+
+
+        }
+
+        public void saveButtonOnClick(View view) {
+            Term term;
+            if (termID == -1) {
+                int newTermID = repo.getAllTerms().get(repo.getAllTerms().size() - 1).getTermID() + 1;
+                term = new Term(newTermID, editTitle.getText().toString(), editStartDate.getText().toString(), editEndDate.getText().toString());
+                repo.insert(term);
+            }else{
+                term = new Term(termID, editTitle.getText().toString(), editStartDate.getText().toString(), editEndDate.getText().toString());
+                repo.update(term);
+
+            }
+        }}
     }
 
     public void saveButton(View view) {
