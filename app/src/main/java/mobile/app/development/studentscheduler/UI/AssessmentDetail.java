b56a35726018261760b73f4c4d0d12d15a77a2f5
Tanya Selvog <tanyaselvog@gmail.com>
2022-08-13 16:08:11 -0700

    added onCreateOptionsMenu to AssessmentDetail to show menu

--- app/src/main/java/mobile/app/development/studentscheduler/UI/AssessmentDetail.java
+++ app/src/main/java/mobile/app/development/studentscheduler/UI/AssessmentDetail.java
@@ -1,11 +1,23 @@
 package mobile.app.development.studentscheduler.UI;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.ParseException;
+import java.util.Date;
+
+import mobile.app.development.studentscheduler.DB.Repository;
+import mobile.app.development.studentscheduler.Entity.Assessment;
 import mobile.app.development.studentscheduler.R;
 
 public class AssessmentDetail extends AppCompatActivity {
@@ -13,6 +25,9 @@ public class AssessmentDetail extends AppCompatActivity {
     TextView editEndDate;
     String title;
     String date;
+    int assessmentID;
+    Assessment current;
+    Repository repo;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -20,15 +35,36 @@ public class AssessmentDetail extends AppCompatActivity {
             editTitle=findViewById(R.id.tv8);
             title=getIntent().getStringExtra("assessmentName");
             editTitle.setText(title);
-
+            assessmentID = getIntent().getIntExtra("assessmentID", -1);
             editEndDate=findViewById(R.id.editTD1);
             date = getIntent().getStringExtra("assessmentDate");
             editEndDate.setText(date);
-
+            repo = new Repository(getApplication());
 
         }
 
     public void onRadioButtonClicked(View view) {
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_assessments, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+
+            case R.id.deleteAssessment:
+
+                    repo.delete(current);
+                    Toast.makeText(AssessmentDetail.this, "Course was deleted", Toast.LENGTH_LONG).show();
+                }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
 
